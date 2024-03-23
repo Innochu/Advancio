0b96007bfdc708fc105e6811c50b139531e3342b
CHUKWUDI INNOCENT <125125776+Innochu@users.noreply.github.com>
2024-03-23 10:46:37 +0100

    Register/Login/Gender/Annotations

---
+++ AdvansioIntLtd/AuthEntity/Login.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdvansioIntLtd.AuthEntity
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "Enter Username/Email")]
+        public string Email { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Enter Password")]
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
---
+++ AdvansioIntLtd/AuthEntity/Register.cs
@@ -0,0 +1,29 @@
+﻿using AdvansioIntLtd.Enum;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace AdvansioIntLtd.AuthEntity
+{
+    public class Register
+    {
+        [Required(ErrorMessage = "LastName is required")]
+        public string LastName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "FirstName is required")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email address is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "PhoneNumber address is required.")]
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password address is required.")]
+
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+       
+        public Gender Gender { get; set; }
+    }
+}
